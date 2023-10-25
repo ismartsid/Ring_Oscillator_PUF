70fbb0e9de459d7aa7ac1eb3588d39fba0ec9d2e
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2023-10-25 13:00:03 -0700

    Update PUF.v

--- PUF.v
+++ PUF.v
@@ -1,29 +1,23 @@
 module PUF #(parameter RO_modules = 16, CounterGroups = 4)(
-  input        reset,
-  input  [3:0] Cha0,
-  input  [3:0] Cha1,
-  output [3:0] Response
+  input reset, //Reset
+  input  [3:0] Sel0, //select lines for mux
+  input  [3:0] Sel1, //select lines for mux
+  output [3:0] response // output from comparator
 );
 
-(* keep *) wire [63:0] ROwire;
-
-//Counter_group(.Cha0(Cha0), .Cha1(Cha1), .RO_out(ROwire[15:0]), .reset(reset), .Response(Response[0]));
-//Counter_group(.Cha0(Cha0), .Cha1(Cha1), .RO_out(ROwire[31:16]), .reset(reset), .Response(Response[1]));
-//Counter_group(.Cha0(Cha0), .Cha1(Cha1), .RO_out(ROwire[47:32]), .reset(reset), .Response(Response[2]));
-//Counter_group(.Cha0(Cha0), .Cha1(Cha1), .RO_out(ROwire[63:48]), .reset(reset), .Response(Response[3]));
+  (* keep *) wire [63:0] RO_wire;
 
 genvar j;
-
-generate
+generate // replicating counter groups
     for(j=0;j<CounterGroups;j = j +1 )begin : counter_generate
-       (* keep *) Counter_group Counter_group_inst(.Cha0(Cha0), .Cha1(Cha1), .RO_out(ROwire[(j*16)+15:j*16]), .reset(reset), .Response(Response[j]));
+      (* keep *) counter counter_inst(.Sel0(Sel0), .Sel1(Sel1), .RO_out(RO_wire[(j*16)+15:j*16]), .reset(reset), .response(response[j]));
         end
 endgenerate
 
 genvar i;
-generate
+generate // replicating ro modules
     for(i=0; i<RO_modules*CounterGroups; i = i + 1)begin : ro_generate
-       (* keep *) RO_basic RO_basic_inst(.Enable(~reset),.RO_out(ROwire[i]));
+       (* keep *) ring_osc ring_osc_inst(.Enable(~reset),.RO_out(RO_wire[i]));
     end 
 endgenerate
 endmodule
