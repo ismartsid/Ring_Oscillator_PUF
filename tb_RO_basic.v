45a2b9b4307027c356cfc950cd16b5f73ad03a91
Sidhartha <85611307+ismartsid@users.noreply.github.com>
2024-01-29 10:23:13 -0700

    Update tb_RO_basic.v

--- tb_RO_basic.v
+++ tb_RO_basic.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
 module tb_RO;
- reg en,out;
+ reg en, out;
  ring_osc dut(.Enable(en),.RO_out(out));
   
  initial begin
  en = 0;
  #100;
  en = 1;
+ #1000 $finish;
  end
 endmodule
